150f55fbfa8c13ea20efca76c9e7aeceb13c976b
porlando-equinix <porlando@equinix.com>
2024-12-17 21:10:35 +0000

    modified:   tf-metal.tf

--- tf-metal.tf
+++ tf-metal.tf
@@ -1,10 +1,18 @@
 terraform {
   required_providers {
     equinix = {
-      source  = "equinix/equinix"
-      version = "2.10.0"
+      source = "equinix/equinix"
+      version = ">= 1.35.0"
     }
   }
 }
+
+provider "equinix" {
+  client_id     = var.client_id
+  client_secret = var.client_secret
+  auth_token    = var.metal_auth_key
+}
+
+
 
 
