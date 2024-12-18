1a25e678076b8a342ce167db8ae54fb77e6e96d8
porlando-equinix <porlando@equinix.com>
2024-12-18 19:35:57 +0000

    modified:   tf-metal.tf

--- tf-metal.tf
+++ tf-metal.tf
@@ -14,28 +14,12 @@ provider "equinix" {
 }
 
 resource "equinix_metal_device" "svr1" {
-  hostname         = "pat-q4-tf-sv-svr1"
+  hostname         = "pat-q4-tf-ny-svr1"
   plan             = "c3.small.x86"
   metro            = "ny"
   operating_system = "ubuntu_20_04"
   billing_cycle    = "hourly"
   project_id       = "aba4bb63-5932-4203-a297-5338d5e9daa3"
-  user_data        = <<-EOT
-package_update: true
-package_upgrade: true
-package_reboot_if_required: true
-write_files:
-- path /etc/network/interfaces
-append: true 
-content: |
-auto bond0.444
-iface bond0.444 inet static 
-address 10.10.10.1
-netmask: 255.255.255.0
-runcmd:
-- systemctl restart networking
-EOT
-
 }
 resource "equinix_metal_device" "svr2" {
   hostname         = "pat-q4-tf-da-svr2"
@@ -44,22 +28,13 @@ resource "equinix_metal_device" "svr2" {
   operating_system = "ubuntu_20_04"
   billing_cycle    = "hourly"
   project_id       = "aba4bb63-5932-4203-a297-5338d5e9daa3"
+}
 
-  user_data        = <<-EOT
-package_update: true
-package_upgrade: true
-package_reboot_if_required: true
-write_files:
-- path /etc/network/interfaces
-append: true
-content: |
-auto bond0.444
-iface bond0.444 inet static
-address 10.10.10.2
-netmask: 255.255.255.0
-runcmd:
-- systemctl restart networking
-EOT
+resource "equinix_metal_port_vlan_attachment" "test" {
+#  count     = local.device_count
+  device_id = equinix_metal_device.svr1.id
+  port_name = "bond0"
+  vlan_vnid = equinix_metal_vlan.vlan1.vxlan
 }
 
 
@@ -93,7 +68,7 @@ resource "equinix_metal_port" "DA-port" {
 resource "equinix_metal_virtual_circuit" "NY-to-dedicated-port" {
   connection_id = ""
   project_id = "aba4bb63-5932-4203-a297-5338d5e9daa3"
-  port_id = ""
+  port_id = "2a027f9f-dae6-454b-a783-2a25c678f506"
   vlan_id = equinix_metal_vlan.vlan1.id
   nni_vlan = 444
 }
@@ -104,7 +79,7 @@ resource "equinix_metal_connection" "pat-tf-fabric-da" {
   type               = "shared"
   redundancy         = "primary"
   metro              = "da"
-  speed              = "50Mbps"
+  speed              = "10Gbps"
   service_token_type = "z_side"
   contact_email      = "porlando@equinix.com"
   vlans              = [equinix_metal_vlan.vlan2.vxlan]
@@ -117,7 +92,7 @@ resource "equinix_fabric_connection" "pat-tf-fabric" {
     type   = "ALL"
     emails = ["porlando@equinix.com"]
   }
-  bandwidth = 50
+  bandwidth = 10000
   a_side {
     access_point {
       type = "COLO"
