ef0b971605cfaf4b1a98f13def0c9937a0b6cb3c
porlando-equinix <porlando@equinix.com>
2024-12-18 18:58:16 +0000

    modified:   tf-metal.tf

--- tf-metal.tf
+++ tf-metal.tf
@@ -20,6 +20,22 @@ resource "equinix_metal_device" "svr1" {
   operating_system = "ubuntu_20_04"
   billing_cycle    = "hourly"
   project_id       = "aba4bb63-5932-4203-a297-5338d5e9daa3"
+  user_data        = <<-EOT
+package_update: true
+package_upgrade: true
+package_reboot_if_required: true
+write_files:
+- path /etc/network/interfaces
+append: true 
+content: |
+auto bond0.444
+iface bond0.444 inet static 
+address 10.10.10.1
+netmask: 255.255.255.0
+runcmd:
+- systemctl restart networking
+EOT
+
 }
 resource "equinix_metal_device" "svr2" {
   hostname         = "pat-q4-tf-da-svr2"
@@ -28,6 +44,22 @@ resource "equinix_metal_device" "svr2" {
   operating_system = "ubuntu_20_04"
   billing_cycle    = "hourly"
   project_id       = "aba4bb63-5932-4203-a297-5338d5e9daa3"
+
+  user_data        = <<-EOT
+package_update: true
+package_upgrade: true
+package_reboot_if_required: true
+write_files:
+- path /etc/network/interfaces
+append: true
+content: |
+auto bond0.444
+iface bond0.444 inet static
+address 10.10.10.2
+netmask: 255.255.255.0
+runcmd:
+- systemctl restart networking
+EOT
 }
 
 
@@ -78,5 +110,27 @@ resource "equinix_metal_connection" "pat-tf-fabric-da" {
   vlans              = [equinix_metal_vlan.vlan2.vxlan]
 }
 
-
+resource "equinix_fabric_connection" "pat-tf-fabric" {
+  name = "pat-tf-fabric"
+  type = "EVPL_VC"
+  bandwidth = 50
+  a_side {
+    access_point {
+      type = "COLO"
+      port {
+        uuid = ""
+      }
+      link_protocol {
+        type = "QINQ"
+        vlan_s_tag = "444"
+      }
+    }
+  }
+  z_side {
+    service_token {
+      uuid = equinix_metal_connection.pat-tf-fabric-da.service_token.0.id
+    }
+  }
+  depends_on = {equinix_metal_connections.pat-tf-fabric-da}
+}
 
