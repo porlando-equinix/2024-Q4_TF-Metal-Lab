ee4e9289f48cd87b9c5a1d645a390464509c324b
porlando-equinix <porlando@equinix.com>
2024-12-18 17:31:12 +0000

    modified:   tf-metal.tf

--- tf-metal.tf
+++ tf-metal.tf
@@ -32,27 +32,27 @@ resource "equinix_metal_device" "svr2" {
 
 
 resource "equinix_metal_vlan" "vlan1" {
-  description = "VLAN in "NY"
+  description = "VLAN in NY"
   metro       = "ny"
-  project_id  = "bdc6d5d4-91d3-44e1-a44b-cb5beda4b4c6"
+  project_id  = "aba4bb63-5932-4203-a297-5338d5e9daa3"
   vxlan       = 444
 }
 resource "equinix_metal_vlan" "vlan2" {
   description = "VLAN in DA"
   metro       = "da"
-  project_id  = "bdc6d5d4-91d3-44e1-a44b-cb5beda4b4c6"
+  project_id  = "aba4bb63-5932-4203-a297-5338d5e9daa3"
   vxlan       = 444
 }
 
 resource "equinix_metal_port" "NY-port" {
   depends_on  = [equinix_metal_device.svr1]
-  port_id     = [2a027f9f-dae6-454b-a783-2a25c678f506]
+  port_id     = "2a027f9f-dae6-454b-a783-2a25c678f506"
   vlan_ids    = [equinix_metal_vlan.vlan1.id]
   bonded      = true
 }
 resource "equinix_metal_port" "DA-port" {
   depends_on  = [equinix_metal_device.svr2]
-  port_id     = [c3b18488-4d4a-4764-9330-a67521d8ed6a]
+  port_id     = "c3b18488-4d4a-4764-9330-a67521d8ed6a"
   vlan_ids    = [equinix_metal_vlan.vlan2.id]
   bonded      = true
 }
@@ -66,6 +66,17 @@ resource "equinix_metal_virtual_circuit" "NY-to-dedicated-port" {
   nni_vlan = 444
 }
 
+resource "equinix_metal_connection" "pat-tf-fabric-da" {
+  name               = "pat-tf-fabric-da"
+  project_id         = "aba4bb63-5932-4203-a297-5338d5e9daa3"
+  type               = "shared"
+  redundancy         = "primary"
+  metro              = "da"
+  speed              = "50Mbps"
+  service_token_type = "z_side"
+  contact_email      = "porlando@equinix.com"
+  vlans              = [equinix_metal_vlan.vlan2.vxlan]
+}
 
 
 
